e611615a329c3e788118e8ac4f4679c739ae2915
ckmpk2004 <44965887+ckmpk2004@users.noreply.github.com>
2019-08-30 02:14:29 +0800

    bug fix

--- index.js
+++ index.js
@@ -47,4 +47,4 @@ cors_proxy.createServer({
     console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
 
-//app.listen(listen_port, () => console.log('Backend server start up at port '+ listen_port));
+app.listen(listen_port, () => console.log('Backend server start up at port '+ listen_port));

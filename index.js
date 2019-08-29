dd8436e3dbc29e6194b871d05dfd617f837132d1
ckmpk2004 <44965887+ckmpk2004@users.noreply.github.com>
2019-08-30 06:54:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 //cors options
 const corsConfig = {
     origin: ["http://localhost:4200"],
-    allowedHeaders: ['Authorization','Access-Control-Allow-Origin'],
+    allowedHeaders: ['Authorization','Access-Control-Allow-Origin','Content-Type'],
     credentials: true,
     methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE"
 };

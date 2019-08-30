0c6bc2aa828fc9c61a1d024c8309e90929d0819a
ckmpk2004 <44965887+ckmpk2004@users.noreply.github.com>
2019-08-30 10:10:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use('/', authRoute, function(req, res, next){
     next();
 });
 app.use('/games', gameRoute, function(req, res, next){
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.set({'Access-Control-Allow-Origin': '*'});
     next();
 });
 

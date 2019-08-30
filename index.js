8ceb1fe427b9cbbd439c466ae09345125a503869
ckmpk2004 <44965887+ckmpk2004@users.noreply.github.com>
2019-08-30 09:24:08 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const mongoose = require('mongoose');
 var bodyParser= require('body-parser');
 const cors = require('cors');
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
 
 //cors options
 const corsConfig = {
